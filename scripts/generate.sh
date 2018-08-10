db9ca36e16f5b7753ea51864710286e3a50e4890
Yatharth Agarwal <yatharth999@gmail.com>
2018-08-09 20:19:40 -0700

    Move docker commands to generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,134 +1,56 @@
 #!/bin/bash
 
 echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
-
 echo "RUN apt-get update"
-
-if [ ! -e $RUBY_VERSION_NUM ] ; then
-    echo "RUN apt-get install -y libssl-dev && wget http://ftp.ruby-lang.org/pub/ruby/$(awk -F'.' '{ print $1"."$2 }' <<< $RUBY_VERSION_NUM)/ruby-$RUBY_VERSION_NUM.tar.gz && \
-    tar -xzvf ruby-$RUBY_VERSION_NUM.tar.gz && \
-    cd ruby-$RUBY_VERSION_NUM/ && \
-    ./configure && \
-    make -j4 && \
-    make install && \
-    ruby -v"
-fi
-
-if [ ! -e $NODE_VERSION_NUM ] ; then
-    echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION_NUM/node-v$NODE_VERSION_NUM.tar.gz && \
-    tar -xzvf node-v$NODE_VERSION_NUM.tar.gz && \
-    rm node-v$NODE_VERSION_NUM.tar.gz && \
-    cd node-v$NODE_VERSION_NUM && \
-    ./configure && \
-    make -j4 && \
-    make install && \
-    cd .. && \
-    rm -r node-v$NODE_VERSION_NUM"
-fi
-
-if [ ! -e $PYTHON_VERSION_NUM ] ; then
-    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
-    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
-    rm Python-$PYTHON_VERSION_NUM.tgz && \
-    cd Python-$PYTHON_VERSION_NUM && \
-    ./configure && \
-    make install"
-fi
-
-# if [ ! -e $PHP_VERSION_NUM ] ; then
-#     wget "http://php.net/distributions/php-${PHP_VERSION_NUM}.tar.xz"
-# fi
-
-if [ $JAVA = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; fi
-EOF
-fi
-
-if [ $MYSQL_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install mysql-client"
-fi
-
-if [ $POSTGRES_CLIENT = "true" ] ; then
-    echo "RUN apt-get -y install postgresql-client"
-fi
-
-if [ $DOCKERIZE = "true" ] ; then
-DOCKERIZE_VERSION="v0.6.1"
-
-cat << EOF
-RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz && \\
-    rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-EOF
-fi
-
-# install bats for testing
-echo "RUN git clone https://github.com/sstephenson/bats.git \
-  && cd bats \
-  && ./install.sh /usr/local \
-  && cd .. \
-  && rm -rf bats"
-
-# install dependencies for tap-to-junit
-echo "RUN perl -MCPAN -e 'install TAP::Parser'"
-echo "RUN perl -MCPAN -e 'install XML::Generator'"
-
 # install lsb-release, etc., for testing linux distro
 echo "RUN apt-get update && apt-get -y install lsb-release unzip"
 
-if [ $BROWSERS = "true" ] ; then
 cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports xvfb phantomjs \\
-; else \\
-		apt-get update && apt-get -y install xvfb phantomjs \\
-; fi
+
+# Prepare to install dependencies.
+ENV DEBIAN_FRONTEND noninteractive
+CMD mkdir ~/logs
+CMD apt-get -yq update &>> ~/logs/apt-get-update.log
+CMD apt-get -yq install --no-install-suggests --no-install-recommends software-properties-common
+CMD apt-get -yq install --no-install-suggests --no-install-recommends curl
+
+# Install dependencies to build C++ core.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install build-essential  # make.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install libopencv-dev valgrind
+CMD apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
+CMD apt-get -yq update &>> ~/logs/apt-get-update.log
+
+# Download cmake (stored at ~/cmake/bin/cmake).
+ADD https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.sh /cmake.sh
+RUN mkdir /opt/cmake
+RUN sh /cmake.sh --prefix=/opt/cmake --skip-license
+RUN ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+RUN cmake --version
+
+CMD curl -s https://cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.tar.gz > ~/cmake.tar.gz
+CMD mkdir ~/cmake
+CMD tar -x -f ~/cmake.tar.gz -C ~/cmake --strip-components=1
+
+# Install R.
+CMD echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" | tee -a /etc/apt/sources.list > /dev/null
+CMD gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+CMD gpg -a --export E084DAB9 | apt-key add -
+CMD apt-get -yq update &>> ~/logs/apt-get-update.log
+CMD apt-get -yq --no-install-suggests --no-install-recommends install r-base
+
+# Install dependencies to build R.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install libcurl4-gnutls-dev libssl-dev  # For devtools.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install libxml2-dev libxslt-dev  # For roxygen.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install libgfortran-8-dev liblapack-dev liblapack3 libopenblas-base libopenblas-dev  # For RcppEigen.
+CMD R -e 'install.packages(c("Rcpp", "devtools", "testthat", "roxygen2", "DiceKriging", "lmtest", "sandwich", "RcppEigen"), repos="http://cran.us.r-project.org")'
+
+# Install clang.
+CMD curl -sSL "http://apt.llvm.org/llvm-snapshot.gpg.key" | apt-key add -
+CMD echo "deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.7 main" | tee -a /etc/apt/sources.list > /dev/null
+CMD apt-get -yq update &>> ~/logs/apt-get-update.log
+CMD apt-get -yq --no-install-suggests --no-install-recommends install clang-3.7
+
+# Install g++.
+CMD apt-get -yq --no-install-suggests --no-install-recommends install g++-4.9
+
 EOF
-echo "ENV DISPLAY :99"
-
-echo "# install firefox
-RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.deb https://s3.amazonaws.com/circle-downloads/firefox-mozilla-build_47.0.1-0ubuntu1_amd64.deb \
-  && echo 'ef016febe5ec4eaf7d455a34579834bcde7703cb0818c80044f4d148df8473bb  /tmp/firefox.deb' | sha256sum -c \
-  && dpkg -i /tmp/firefox.deb || apt-get -f install  \
-  && apt-get install -y libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3 \
-  && rm -rf /tmp/firefox.deb"
-
-echo "# install chrome
-RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-  && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || apt-get -fy install)  \
-  && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
-  && sed -i 's|HERE/chrome\"|HERE/chrome\" --disable-setuid-sandbox --no-sandbox|g' \
-       \"/opt/google/chrome/google-chrome\""
-
-echo "# install chromedriver
-RUN apt-get -y install libgconf-2-4 \
-  && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip \"http://chromedriver.storage.googleapis.com/2.33/chromedriver_linux64.zip\" \
-  && cd /tmp \
-  && unzip chromedriver_linux64.zip \
-  && rm -rf chromedriver_linux64.zip \
-  && mv chromedriver /usr/local/bin/chromedriver \
-  && chmod +x /usr/local/bin/chromedriver"
-fi
